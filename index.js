b765eb1f9c0e163c57de7c99dc8b7b26503274a8
Namima Islam <namimaislam308@gmail.com>
2024-07-04 22:14:58 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution here
+//findMatching()
+//returns all drivers that match the passed in name
+//returns matching drivers if case does not match but letters do
+//returns an empty array if there is no match
+    
+//fuzzyMatch()
+//returns a driver if beginning provided letters match
+//does not return drivers if only middle or ending letters match
+//does not return drivers if only middle or ending letters match
+  
+//matchName()
+//accesses the data structure to check if name matches
+
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"];
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+findMatching(drivers);
+
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+}
